e82d8e7df4a02d4abbf550e9ddfbee4bf723dc81
Qurbonov Muxiddin <uzzzu91@gmail.com>
2026-02-28 02:00:06 +0500

    Add files via upload

---
+++ homework19.py
@@ -0,0 +1,50 @@
+#1.Kvadratlar jadvali: 1 dan 20 gacha bo'lgan sonlarning kvadratini (i**2) ekranga jadval ko'rinishida chiqaring
+# for i in range(1,21,1):
+#     i=i**2
+#     print(i)
+
+
+# 2.Foydalanuvchidan son so'raymiz va shu sondan 1 ga cha bolgan sonlarni chiqaramiz ( 100 => 100,99,98 ....1) ( 5 => 5,4,3,2,1)
+
+# x=int(input("son kiriting: "))
+# for i in range(x,0,-1):
+#     print(i)
+
+
+
+# 3.  Foydalanuvchidan son so'ralsi9n va shu sonni 1 dan 10 gacha ko'paytmasi chiqarilsin ( 5 => 5 * 1 = 5, 5 * 2 = 10 , 15,20 .... 45,50) ( 7 => 7,14,21,28 ... 63,70)
+
+# x=int(input("son kiriting: "))
+
+# for i in range(1,11,1):
+#     print(i*x)
+
+
+
+
+#4.Faktorialni topish: Berilgan musbat butun sonning faktorialini (masalan, 5! = 1*2*3*4*5) for yordamida hisoblaydigan dastur tuzing 
+
+
+# x=int(input("son kiriting: "))
+
+# for i in range(1,x):
+#     x=x*i
+#     print(x)
+    
+
+# 5. Tub sonlarni aniqlash: 1 dan 100 gacha bo'lgan oraliqdagi barcha tub sonlarni (faqat o'ziga va 1 ga bo'linadigan sonlar) topib chiqaring  (2,3,5,7 ... 97)
+
+#i don't fully understand this ☹
+
+
+
+# 6. Yulduzchali piramida: Ichma-ich tsikllar yordamida quyidagi shaklni hosil qiling (int)
+
+
+
+# x=int(input("son kiriting: "))
+
+# for i in range(1,x):
+#     for k in range(i):
+#         print("*",end=" ")
+#     print()
