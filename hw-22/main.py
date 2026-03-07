b9dc872772e976c5b07297a1fe631db29e51ea25
Qurbonov Muxiddin <uzzzu91@gmail.com>
2026-03-07 14:45:09 +0500

    homework22.py

--- hw-22/main.py
+++ hw-22/main.py
@@ -1,9 +1,48 @@
-1.Berilgan gapdagi barcha raqamlarni olib tashlang, faqat harflar qolsin.
-2.Foydalanuvchi kiritgan so‘z palindrom ekanligini tekshiring.
-3. Matndagi ortiqcha bo‘shliqlarni bittaga aylantiring. ("Assalom    aleykum  aasda" => "Assalom aleykum aasda")
-4. So‘zlar ichidagi unli harflarni “*” belgisi bilan almashtiring.
-5. Berilgan gapdagi so‘zlarni teskari tartibda joylashtiring, lekin har bir so‘z o'rni o‘zgarishsiz qolsin.
-Masalan:
-  # "Assalom aleykum yurtdoshim"
-  # "mishodtruy mukyela molassA" => xato bunday emas
-  # "molassA mukyela mishodtruy" => to'gri
+# x=input("soz kriting: ")
+
+# x = x.replace("0","")
+# x = x.replace("1","")
+# x = x.replace("2","")
+# x = x.replace("3","")
+# x = x.replace("4","")
+# x = x.replace("5","")
+# x = x.replace("6","")
+# x = x.replace("7","")
+# x = x.replace("8","")
+# x = x.replace("9","")
+
+# print(x)
+
+
+
+
+
+
+
+# x=input("sozni kriting: ")
+
+# y=""
+
+# for i in x:
+#     y=i+y
+# if x == y:
+#     print("palindrom")
+# else:
+#     print("palindrom emas")
+
+
+
+
+
+
+
+# x=input("soz kriting: ")
+
+# x = x.replace("a","*")
+# x = x.replace("e","*")
+# x = x.replace("i","*")
+# x = x.replace("o","*")
+# x = x.replace("u","*")
+
+
+# print(x)
