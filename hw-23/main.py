40c134d9470f7b6d53d91bba3a49c696b9a747de
Qurbonov Muxiddin <uzzzu91@gmail.com>
2026-03-10 14:26:39 +0500

    Muhiddin-tomonidan_qilind

--- hw-23/main.py
+++ hw-23/main.py
@@ -1,12 +1,103 @@
-Oson daraja (Easy)
-1.Kvadrat hisoblovchi: Son qabul qilib, uning kvadratini konsolga chiqaruvchi funksiya yozing.
-2.Yosh hisoblagich: Tug'ilgan yilni qabul qilib, foydalanuvchining yoshini hisoblab beruvchi funksiya yarating.
-3.Juft yoki toq: Berilgan son juft bo'lsa True, toq bo'lsa False qaytaruvchi funksiya yozing.
-4.Eng kattasi: Ikkita son qabul qilib, ulardan kattasini ekranga chiqaruvchi funksiya tuzing.
-5.Text uzunligi: Text qabul qilib, unda uzunligini chiqaruvchi (tayyor len() dan foydalanmaslikka harakat qiling).
+# 1 Kvadrat hisoblovchi: Son qabul qilib, uning kvadratini konsolga chiqaruvchi funksiya yozing.
 
-O'rta daraja (Medium)
-Unli harflar: Matn qabul qilib, undagi unli harflar sonini hisoblab qaytaruvchi funksiya yozing.
+# x=int(input("son kiriting: "))
+# def kvadrat_hisbolovchi(x,b):
+#     print(x**b)
 
-Parol tekshirgich: Parol (string) qabul qilib, uning uzunligi kamida 8 ta belgidan iboratligini tekshiruvchi funksiya yozing.
-Standart qiymat: Foydalanuvchi ismi so'ralsin va funksiya orqali ("Salom , {ism}) chiqarilsin!
+
+# kvadrat_hisbolovchi(x,2)
+
+
+
+
+
+
+# 2.Yosh hisoblagich: Tug'ilgan yilni qabul qilib, foydalanuvchining yoshini hisoblab beruvchi funksiya yarating.
+
+
+
+# x=int(input("tugilgan yilingizni kiriting: "))
+
+# def tugilgan_yil(a,x):
+#     print(a-x,"yosh")
+
+
+# tugilgan_yil(2026,x)
+
+
+
+
+
+# 3.Juft yoki toq: Berilgan son juft bo'lsa True, toq bo'lsa False qaytaruvchi funksiya yozing.
+
+
+# x=int(input("son kiriting: "))
+
+# def juft_toq(x):
+#     if x%2==0:
+#         print("juft")
+#     else:
+#         print("toq son")
+# juft_toq(x)
+
+
+
+
+
+
+
+# 4.Eng kattasi: Ikkita son qabul qilib, ulardan kattasini ekranga chiqaruvchi funksiya tuzing.
+
+
+
+# x=int(input("son kiriting: "))
+
+# k=int(input("2chi sonni kiriting: "))
+
+
+# def taqqoslash_son(x,k):
+#     if x>k:
+#         print("X katta")
+#     elif x<k:
+#         print("k katta")
+#     else:
+#         print("teng")
+# taqqoslash_son(x,k)
+
+
+
+
+
+# 5.Text uzunligi: Text qabul qilib, unda uzunligini chiqaruvchi (tayyor len() dan foydalanmaslikka harakat qiling).
+
+
+
+# x=input("matn kiriting: ")
+# def text_uzunligi(x):
+#     hisoblash=0
+#     for i in x:
+#         hisoblash+=1
+#     print(hisoblash)
+# text_uzunligi(x)
+
+
+
+
+
+
+#  Unli harflar: Matn qabul qilib, undagi unli harflar sonini hisoblab qaytaruvchi funksiya yozing.
+
+
+#well i couldnt do it
+
+# x=input("matn kiriting: ")
+
+# def unli_harflar(x):
+#     count=0
+#     unlilar="a, e, i, o, u,A,E,I,O,U"
+#     for i in unlilar:
+#         if i in x:
+#             count=countr+1
+# #     print(count)
+# unli_harflar(x)
+            
